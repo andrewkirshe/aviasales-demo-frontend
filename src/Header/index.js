3860de8ed2358dc42d7f582967b9762c4cc01b9d
Andrew Kirshe <andrewkirshe@gmail.com>
2018-02-25 12:09:00 +0200

    resolved conflict with header
    Signed-off-by: Andrew Kirshe <andrewkirshe@gmail.com>

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,21 +1,23 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import Home from "./Home";
-import Search from "./Search";
-import "normalize.css";
-import "flexboxgrid2";
+import React from "react";
+import styled from "styled-components";
+import SearchForm from "../SearchForm";
+import LogoRow from "./LogoRow";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Route path="/" exact component={Home} />{" "}
-          <Route path="/search" component={Search} />{" "}
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const Header = styled.header`
+  background: linear-gradient(to left, #196ebd, #01b0dd);
+  height: ${props => (props.page === "home" ? "100vh" : "auto")};
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+`;
 
-export default App;
+export default props => {
+  return (
+    <Header page={props.page}>
+      <LogoRow page={props.page} />
+      <SearchForm page={props.page} />
+    </Header>
+  );
+};
